628c51f60c233007a8740a7683bbe7f19e7bea0c
Edvinas <edvinasalimas98@gmail.com>
2021-08-30 19:51:33 +0300

    Adding project Online-store as card

--- src/Projects-page/Projects.js
+++ src/Projects-page/Projects.js
@@ -1,14 +1,135 @@
 import React from 'react';
 import './Projects.css';
+import './Untitled.png';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import {red} from "@material-ui/core/colors";
 
-const Projects = () => {
+//
+// const Projects = () => {
+//     return (
+//         <section>
+//             <Card className={classes.root}>
+//                 <CardHeader>
+//                     <h1>Online store</h1>
+//                     </CardHeader>
+//             <div className='centered'>
+//
+//                 <p>
+//                     My application was hosted on heroku.<br/>
+//                     To create this application I used Angular as front-end and Java Spring boot as back-end and <br/>
+//                     the server to get information from is PostgreSQL.
+//                 </p>
+//                 <p>
+//                     In order for the application to fully start, first of all we need to user server link to get it up and <br/>
+//                     running, so here's the server link: <a href='https://working-store.herokuapp.com/' target='_blank' rel="noreferrer">
+//                     https://working-store.herokuapp.com/</a>, once that is done you now can<br/>
+//                     use link to get to login page of the store:<a href='https://working-store.herokuapp.com/' target='_blank' rel="noreferrer">
+//                     https://digitalists.herokuapp.com/login.</a><br/>
+//                     To log in as an admin, the login Username is : admin and the password is admin. <br/>
+//                     To log in as an user, the login Username is : user and the password is user.
+//                 </p>
+//             </div>
+//             </Card>
+//         </section>
+//     );
+// };
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            maxWidth: 345,
+            background: red,
+        },
+        media: {
+            height: 0,
+            paddingTop: '56.25%', // 16:9
+        },
+        expand: {
+            transform: 'rotate(0deg)',
+            marginLeft: 'auto',
+            transition: theme.transitions.create('transform', {
+                duration: theme.transitions.duration.shortest,
+            }),
+        },
+        expandOpen: {
+            transform: 'rotate(180deg)',
+        },
+    }),
+);
+
+export default function Projects () {
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
     return (
-        <section>
-            <div className='centered'>
-                <h1>Online store</h1>
-            </div>
+        <section className='centered'>
+        <Card className={classes.root}>
+            <CardHeader
+                avatar={
+                    <Avatar aria-label="Store" className={classes.avatar}>
+                        {/*<img src={'../../public/Untitled.png'} alt={'a'}/>*/}
+                    </Avatar>
+                }
+                title="Digital sales outlet"
+                subheader="Since 2020"
+            />
+            <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    This is my own project created to the how much I learned
+                    during Software Development Academy. My application was hosted on heroku.
+                    To create this application I used Angular as front-end and Java Spring boot as back-end and
+                    the server to get information from is PostgreSQL
+                </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+                <IconButton
+                    className={clsx(classes.expand, {
+                        [classes.expandOpen]: expanded,
+                    })}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                >
+                    <ExpandMoreIcon />
+                </IconButton>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                    <Typography paragraph></Typography>
+                    <Typography paragraph>
+                        In order for the application to fully start, first of all we need to user server link to get it up and
+                        running, so here's the server link: <a href='https://working-store.herokuapp.com/' target='_blank' rel="noreferrer">
+                        https://working-store.herokuapp.com/</a>, once that is done you now can
+                        use link to get to login page of the store:<a href='https://working-store.herokuapp.com/' target='_blank' rel="noreferrer">
+                        https://digitalists.herokuapp.com/login.</a>
+                        To log in as an user, the login Username is : user and the password is user.
+                    </Typography>
+                    <Typography paragraph>
+                        To log in as an Admin, you can just use Username: admin and the password: admin
+                    </Typography>
+                    <Typography paragraph>
+                        To log in as an user, the login Username is : user and the password is user.
+                    </Typography>
+                </CardContent>
+            </Collapse>
+        </Card>
+            <img alt="Image" src="/src/Projects-page/Untitled.png"/>
         </section>
     );
-};
-
-export default Projects;
+}
+//
+// export default Projects;
