5c77e2af34fbff222e4bdb5c53ff6df099068f79
Edvinas <edvinasalimas98@gmail.com>
2021-09-05 15:11:30 +0300

    Footer fixing

--- src/About-page/About.js
+++ src/About-page/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <section>
-            <div className='centered'>
+            <div className='homePage'>
                 <h1>I'm passionate when it comes to coding,<br/> because it's really interesting and of the mains
                 things and the most important is working out and running!</h1>
             </div>
